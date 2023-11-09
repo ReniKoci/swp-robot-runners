a6186838d9ee2470f2cdd5476021cbe2a97ad8ae
Reni <r.koci98@gmail.com>
2023-11-09 11:04:17 +0100

    encapsulated functions and added benchmark comparisons

--- benchmark.py
+++ benchmark.py
@@ -1,24 +1,25 @@
 import subprocess
 import json
+import os
 import time
+import shutil
+
 
 # compilation
-compile_cmd = "sh compile.sh"
-subprocess.run(compile_cmd, shell=True, check=True)
-
-# running using different maps
-input_files = [
-    "random.domain/random_20.json",
-    "random.domain/random_50.json",
-    # "random_100.json",
-    # "random_200.json"
-]
+def compile_code():
+    compile_cmd = "sh compile.sh"
+    subprocess.run(compile_cmd, shell=True, check=True)
 
 
-def run_code(input_file):
+def generate_filename():
+    return time.strftime("%Y%m%d_%H%M%S")
+
+
+# executing algorithm for each map
+def run_iteration(input_file):
     map_file_location = "./example_problems/" + input_file
 
-    # start exeuction time
+    # start execution time
     start_time = time.time()
     run_cmd = f"./build/lifelong --inputFile {map_file_location} -o test.json"
     subprocess.run(run_cmd, shell=True, check=True)
@@ -33,15 +34,67 @@ def run_code(input_file):
     return num_tasks_finished, execution_time
 
 
-# create a list to store results
-results = []
+def run_code(input_files, output_file_path):
+    # create the directory if it doesnt exist
+    output_directory = os.path.dirname(output_file_path)
+    os.makedirs(output_directory, exist_ok=True)
 
-# run code for each map
-for input_file in input_files:
-    num_task_finished, execution_time = run_code(input_file)
-    results.append({"file": input_file, "tasks_finished": num_task_finished, "execution_time": execution_time})
+    # create a list to store results
+    results = []
+
+    # run code for each map
+    for input_file in input_files:
+        num_task_finished, execution_time = run_iteration(input_file)
+        results.append({"file": input_file, "tasks_finished": num_task_finished, "execution_time": execution_time})
+
+    with open(output_file_path, "a") as output_file:
+        json.dump(results, output_file, indent=4)
 
 
-output_file_path = "benchmark_results.json"
-with open(output_file_path, "w") as output_file:
-    json.dump(results, output_file, indent=4)
+def get_total_tasks_finished(file_name):
+    with open(file_name, "r") as file:
+        data = json.load(file)
+
+    total_tasks_finished = sum(entry.get("tasks_finished", 0) for entry in data)
+    return total_tasks_finished
+
+
+def compare_and_update_best_benchmark(file_name):
+    # if the best benchmark file does not exist, create it
+    if not os.path.isfile("best_benchmark.json"):
+        shutil.copy(file_name, "best_benchmark.json")
+        return
+
+    # get the current number of tasks finished
+    current_benchmark_tasks_finished = get_total_tasks_finished(file_name)
+    best_benchmark_tasks_finished = get_total_tasks_finished("best_benchmark.json")
+
+    if current_benchmark_tasks_finished > best_benchmark_tasks_finished:
+        shutil.copy(file_name, "best_benchmark.json")
+        print("Better benchmark found!")
+
+
+def main():
+    # running using different maps
+    input_files = [
+        "random.domain/random_20.json",
+        "random.domain/random_50.json",
+        # "random_100.json",
+        # "random_200.json"
+    ]
+
+    # compilation
+    # compile_code()
+
+    timestamp_filename = generate_filename()
+    output_file_path = f"Output/benchmark_{timestamp_filename}.json"
+
+    # execution
+    run_code(input_files, output_file_path)
+
+    # check if we found a better algorithm
+    compare_and_update_best_benchmark(output_file_path)
+
+
+if __name__ == "__main__":
+    main()
