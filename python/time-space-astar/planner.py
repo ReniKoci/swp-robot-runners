6d8bce489c44273d91475cb9ef1804b1242a53c7
Reni <r.koci98@gmail.com>
2023-12-02 14:34:20 +0100

    new branch, some helper classes and functions

---
+++ python/time-space-astar/planner.py
@@ -0,0 +1,21 @@
+# these will the functions that we can directly call from the "init" and "plan" function
+# on the pyMAPFPlanner.py file
+
+class Planner:
+    def __init__(self):
+        pass
+
+    def initialize(self):
+        pass
+
+    def plan(self):
+        pass
+
+    # ------ helper functions ---------
+
+    # Max gonna implement a function and we can call it here
+    def astar_distance(self):
+        pass
+
+    def space_time_astar(self):
+        pass
---
+++ python/time-space-astar/state.py
@@ -0,0 +1,35 @@
+# directly copied from the Space-Time-AStar repo
+# https://github.com/GavinPHR/Space-Time-AStar/blob/master/stastar/state.py
+
+# this will be the state of robots at a certain time (I think)
+
+import numpy as np
+
+
+class State:
+
+    def __init__(self, pos: np.ndarray, time: int, g_score: int, h_score: int):
+        self.pos = pos
+        self.time = time
+        self.g_score = g_score
+        self.f_score = g_score + h_score
+
+    def __hash__(self) -> int:
+        concat = str(self.pos[0]) + str(self.pos[1]) + '0' + str(self.time)
+        return int(concat)
+
+    def pos_equal_to(self, pos: np.ndarray) -> bool:
+        return np.array_equal(self.pos, pos)
+
+    def __lt__(self, other: 'State') -> bool:
+        return self.f_score < other.f_score
+
+    def __eq__(self, other: 'State') -> bool:
+        return self.__hash__() == other.__hash__()
+
+    def __str__(self):
+        return 'State(pos=[' + str(self.pos[0]) + ', ' + str(self.pos[1]) + '], ' \
+            + 'time=' + str(self.time) + ', fscore=' + str(self.f_score) + ')'
+
+    def __repr__(self):
+        return self.__str__()
